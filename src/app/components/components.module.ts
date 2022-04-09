8f0c8497aa16371e101e11c006fdb0eb4b34069a
Altamirano Edgar Gastón <edgargastonaltamirano@gmail.com>
2022-04-09 16:59:23 -0300

    Fix de import de IonicModule en el card

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { HeaderComponentComponent } from '../header-component/header-component.component';
 import { CardComponent } from './card/card.component';
 
 @NgModule({
-  imports:[CommonModule],
+  imports:[CommonModule,IonicModule],
   declarations:[CardComponent, HeaderComponentComponent],
   exports:[CardComponent, HeaderComponentComponent,CommonModule]
 })
