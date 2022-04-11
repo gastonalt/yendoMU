62fd7258d329e1793a5ca48d42f7cde16378e04a
Altamirano Edgar Gastón <edgargastonaltamirano@gmail.com>
2022-04-10 21:04:02 -0300

    Abstracción del array de eventos, Cambios en la estructura de los objetos evento. Aún se llaman 'card' debo cambiar esto.

--- src/app/pages/reservas-activas/reservas-activas.page.ts
+++ src/app/pages/reservas-activas/reservas-activas.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { cards } from '../../prototypeData';
 
 @Component({
   selector: 'app-reservas-activas',
@@ -9,16 +10,7 @@ import { Storage } from '@ionic/storage';
 })
 export class ReservasActivasPage implements OnInit {
 
-  bolicheYendo = {
-    title: 'Boliche Yendo',
-    descripcion: 'Este es el único boliche donde se puede reservar en esta prueba :) Lo encontraste!',
-    backgroundUrl: 'https://i0.wp.com/elpilon.com.co/wp-content/uploads/2018/04/party-wallpape.jpg?fit=800%2C500&ssl=1',
-    ballColor: 'transparent',
-    fecha: '11/04/22',
-    time: 21,
-  };
-  cards= [
-  ];
+  cards = cards;
 
   constructor(private router: Router, private storage: Storage) { }
   navigate(bolicheYendo: any){
@@ -35,6 +27,8 @@ export class ReservasActivasPage implements OnInit {
     this.storage.get('reservado').then((boliches)=>{
       if(boliches){
         this.cards = boliches;
+      }else{
+        this.cards = [];
       }
     });
   }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { cards } from '../prototypeData';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
@@ -10,31 +11,7 @@ import { Storage } from '@ionic/storage';
 })
 export class Tab1Page implements OnInit{
 
-  cards= [
-    {
-      title: 'Boliche Yendo',
-      descripcion: 'Este es el único boliche donde se puede reservar en esta prueba :) Lo encontraste!',
-      backgroundUrl: 'https://i0.wp.com/elpilon.com.co/wp-content/uploads/2018/04/party-wallpape.jpg?fit=800%2C500&ssl=1',
-      ballColor: '#5FB8A1',
-      fecha: '11/04/22',
-      time: 21,
-    },
-  {
-    title: 'La fiesta de la Cerveza',
-    descripcion: 'Te invitamos a compartir una de las mejores fiestas en esta increible ocacion con invitados de lujo!!!',
-    backgroundUrl: 'https://media.istockphoto.com/photos/dancing-friends-picture-id501387734?k=20&m=501387734&s=612x612&w=0&h=1mli5b7kpDg428fFZfsDPJ9dyVHsWsGK-EVYZUGWHpI=',
-    ballColor: '#E65768',
-    fecha: '27/01/22',
-    time: 22,
-  },{
-    title: 'L4Rmx`s Party',
-    descripcion: 'Una fiesta inigulable que te llenará de buenos recuerdos, ideal para venir con esa persona especial!!!',
-    backgroundUrl: 'https://media.istockphoto.com/photos/people-enjoying-house-party-picture-id936276840?k=20&m=936276840&s=170667a&w=0&h=67_-ZC6VHkYUiuGJ7NPrMuWIjdJ_EWr8IoKmghYbF4Y=',
-    ballColor: '#FAD566',
-    fecha: '25/04/21',
-    time: 18,
-  }
-  ];
+  cards = cards;
 
   constructor(public alertController: AlertController,
               private router: Router,
